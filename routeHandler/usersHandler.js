8c5463ef5c2bd3d5fe7ca3357e5f3ace5b41ae1e
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-08-28 18:33:45 +0600

    add

--- routeHandler/usersHandler.js
+++ routeHandler/usersHandler.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const usersSchema = require('../schemas/userSchema')
 
 //Get all the users
 router.get('/', async(req, res)=>{
