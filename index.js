3101d23b6d1cfdb5db9e6cbdc810de5ddabdf301
Shafaet-j <lbshaft01@gmail.com>
2023-09-20 23:20:13 +0600

    doctor route api done

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const usersHandler = require('./routes/userRoute')
+const express = require("express");
+const mongoose = require("mongoose");
+const usersHandler = require("./routes/userRoute");
+const doctorsHandler = require("./routes/doctorsRoute");
 const cors = require("cors");
-const databaseConnect = require('./config/database')
+const databaseConnect = require("./config/database");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(cors());
-databaseConnect()
+databaseConnect();
 
 // application routes
-app.use('/users', usersHandler)
+app.use("/users", usersHandler);
+app.use("/doctors", doctorsHandler);
 
-app.get('/', (req, res) => {
-    res.send('server is running')
-})
-
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 //default error handler
 function errorHandler(err, req, res, next) {
-    if (res.headersSend) {
-        return next(err)
-    }
-    res.status(500).json({ error: err })
+  if (res.headersSend) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
 }
 
 app.listen(5000, () => {
-    console.log(`app listening at port 5000`)
-})
+  console.log(`app listening at port 5000`);
+});
