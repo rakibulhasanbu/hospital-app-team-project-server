67e2654a3ed66f79f110d1aea4026f5d198acc1c
Shafaet-j <lbshaft01@gmail.com>
2023-11-01 20:34:46 +0600

    products route added

---
+++ models/productModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = productSchema;
